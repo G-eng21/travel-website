34bc4b9f0fe1355c1df6652a124e2c0a43a29851
G-eng21 <rgevans721@yahoo.co.uk>
2025-09-08 17:36:46 +0100

    Plan feature added to allow selection of an itinerary and update the two lists from an object and an array

--- plan.js
+++ plan.js
@@ -11,7 +11,10 @@ const destinations = [
   { location: "Cairns", photo: "explorelink", territory: "Eastern" },
 ];
 
-const choices = [];
+const itineraryChoices = [];
+
+const destinationTableInfo = document.getElementsByClassName("plan-table__row");
+console.log(destinationTableInfo);
 
 const destinationList = document.getElementsByClassName(
   "plan-table__item--destination"
@@ -28,6 +31,11 @@ const exploreList = document.getElementsByClassName(
 );
 // console.log(exploreList);
 
+const itineraryList = document.querySelectorAll(".plan-itinerary__item");
+// console.log(itineraryList);
+
+const itineraryButton = document.querySelector("#save-itinerary");
+
 let index = 0;
 
 for (const destination of destinations) {
@@ -37,16 +45,39 @@ for (const destination of destinations) {
   index++;
 }
 
+itineraryButton.addEventListener("submit", function (event) {
+  console.log("submit event in plan.js");
+  event.preventDefault();
+});
+
 document.addEventListener("keyup", (event) => {
   const keyName = event.key;
 
   if (keyName === "Enter") {
     console.log(event.target.value);
-    choices.push(event.target.value);
-    for (const item of destinationList) {
-      if (item.value === event.target.value) {
-        item.classList.add("hidden");
+    itineraryChoices.push(event.target.value);
+    // for (let index2 = 0; index2 <= destinationList.length; index2++) {
+    let index2 = 0;
+    for (const destination of destinationList) {
+      // console.log(destination.value);
+      if (destination.value === event.target.value) {
+        // console.log(destinationTableInfo[index2]);
+
+        destinationTableInfo[index2].classList.add("hidden");
+        break;
       }
+      index2++;
+    }
+
+    let index3 = 0;
+
+    if (itineraryChoices) {
+      for (const itinerary of itineraryChoices) {
+        itineraryList[index3].textContent = itinerary;
+        itineraryList[index3].classList.remove("hidden");
+        index3++;
+      }
+      itineraryButton.removeAttribute("disabled");
     }
   }
 });
